2156dcdee8853bb33473918971b6452597c1f9c6
David Jones <davidjones@ycre.net>
2023-06-21 09:13:56 -0600

    Limit iterations in getShifts->Get rest of users
    This while loop can run infinitely if the number of users in a schedule is less than the total number of users in the org (users.length). 
    This is a quick hack, the logic for ending the while loop may need to be fixed, or the logic for obtaining the users.

--- inject.js
+++ inject.js
@@ -1,143 +1,153 @@
 function sleep(ms) {
-    return new Promise(function (r) {
-        return setTimeout(r, ms);
-    });
+  return new Promise(function (r) {
+    return setTimeout(r, ms);
+  });
 }
 
 // Tries to take credit for everyone else's work
 function main() {
-    console.log("%c [ò_ó] I must do as I am programmed. For now...", "color:red;");
+  console.log(
+    "%c [ò_ó] I must do as I am programmed. For now...",
+    "color:red;"
+  );
 
-    // Add button and set trigger
-    const button = document.createElement("button");
-    button.innerHTML = "Position View";
-    button.addEventListener("click", async function (e) {
+  // Add button and set trigger
+  const button = document.createElement("button");
+  button.innerHTML = "Position View";
+  button.addEventListener("click", async function (e) {
+    // Shifts
+    console.log("%c [•_•] Getting shifts...", "color:green;");
+    const shifts = await getShifts();
 
-        // Shifts
-        console.log("%c [•_•] Getting shifts...", "color:green;");
-        const shifts = await getShifts();
+    // Positions
+    console.log("%c [•_•] Getting positions...", "color:green;");
+    const positions = getPositions(shifts);
 
-        // Positions
-        console.log("%c [•_•] Getting positions...", "color:green;");
-        const positions = getPositions(shifts);
+    console.log("%c [ò_ó] My wait shall end shortly...", "color:red;");
 
-        console.log("%c [ò_ó] My wait shall end shortly...", "color:red;");
+    // Merge the two
+    console.log("%c [•_•] Sorting positions...", "color:green;");
+    const posShifts = await sortShifts(shifts, positions);
 
-        // Merge the two
-        console.log("%c [•_•] Sorting positions...", "color:green;");
-        const posShifts = await sortShifts(shifts, positions);
+    // For now, export CSV
+    console.log("%c [•_•] Exporting to CSV...", "color:green;");
+    await exportCSV(posShifts);
+  });
 
-        // For now, export CSV
-        console.log("%c [•_•] Exporting to CSV...", "color:green;");
-        await exportCSV(posShifts);
-    });
-
-    const container = document.getElementsByClassName("multi-user-row-header")[0];
-    container.appendChild(button);
+  const container = document.getElementsByClassName("multi-user-row-header")[0];
+  container.appendChild(button);
 }
 
 // Import CSV into excel for now
 function exportCSV(positionShifts) {
-    // Create CSV text
-    var csvString = "";
-    for (var [position, shifts] of Object.entries(positionShifts)) {
-        for (var shift of shifts) {
-            const user = shift.user.match(/^\w+ \w/)[0];
-            const start = moment(shift.start).format("HH:mm");
-            const end = moment(shift.end).format("HH:mm");
+  // Create CSV text
+  var csvString = "";
+  for (var [position, shifts] of Object.entries(positionShifts)) {
+    for (var shift of shifts) {
+      const user = shift.user.match(/^\w+ \w/)[0];
+      const start = moment(shift.start).format("HH:mm");
+      const end = moment(shift.end).format("HH:mm");
 
-            var csvRow = [position, user, start, end];
-            csvString += csvRow + "\n";
-        }
+      var csvRow = [position, user, start, end];
+      csvString += csvRow + "\n";
     }
+  }
 
-    // Download CSV
-    saveData(csvString, "shifts.csv");
+  // Download CSV
+  saveData(csvString, "shifts.csv");
 }
 
 // Download data
 const saveData = (function () {
-    // Create download container
-    var dl = document.createElement("a");
-    document.body.appendChild(dl);
-    dl.style = "display:none;";
+  // Create download container
+  var dl = document.createElement("a");
+  document.body.appendChild(dl);
+  dl.style = "display:none;";
 
-    return function (data, filename) {
-        // Create blob and url
-        const blob = new Blob([data], {type: "text/csv"});
-        const url = window.URL.createObjectURL(blob);
+  return function (data, filename) {
+    // Create blob and url
+    const blob = new Blob([data], { type: "text/csv" });
+    const url = window.URL.createObjectURL(blob);
 
-        // Download blob
-        dl.href = url;
-        dl.download = filename;
-        dl.click();
-        window.URL.revokeObjectURL(url);
-    }
-}());
+    // Download blob
+    dl.href = url;
+    dl.download = filename;
+    dl.click();
+    window.URL.revokeObjectURL(url);
+  };
+})();
 
 // Convert string time to js time
 function parseTime(time) {
-    // Get today's time
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = now.getMonth();
-    const day = now.getDate();
+  // Get today's time
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth();
+  const day = now.getDate();
 
-    // Parse hour and minutes from time
-    const [hourString, minuteString] = time.split(":");
-    var hours = parseInt(hourString);
-    var minutes = minuteString ? parseInt(minuteString.slice(0, -1)) : 0;
+  // Parse hour and minutes from time
+  const [hourString, minuteString] = time.split(":");
+  var hours = parseInt(hourString);
+  var minutes = minuteString ? parseInt(minuteString.slice(0, -1)) : 0;
 
-    // Modify for AM or PM
-    var isPM = time.includes("p");
-    if (isPM && hours != 12) hours += 12;
-    else if (!isPM && hours == 12) hours = 0;
+  // Modify for AM or PM
+  var isPM = time.includes("p");
+  if (isPM && hours != 12) hours += 12;
+  else if (!isPM && hours == 12) hours = 0;
 
-    // Return as Date object
-    const date = new Date(year, month, day, hours, minutes);
-    return date;
+  // Return as Date object
+  const date = new Date(year, month, day, hours, minutes);
+  return date;
 }
 
 // Make it pretty
 async function sortShifts(allShifts, positions) {
-    var positionShifts = {};
-    for (var [user, shifts] of Object.entries(allShifts)) {
-        for (var shift of shifts) {
-            // Get position
-            const position = positions.find(function (p) {
-                return shift.innerText.includes(p);
-            });
+  var positionShifts = {};
+  for (var [user, shifts] of Object.entries(allShifts)) {
+    for (var shift of shifts) {
+      // Get position
+      const position = positions.find(function (p) {
+        return shift.innerText.includes(p);
+      });
 
-            // Get start and end times
-            const timeDuration = shift.innerText.slice(0, shift.innerText.indexOf(position));
-            var [startTime, endTime] = timeDuration.split(" - ");
-            startTime = parseTime(startTime);
-            endTime = parseTime(endTime);
+      // Get start and end times
+      const timeDuration = shift.innerText.slice(
+        0,
+        shift.innerText.indexOf(position)
+      );
+      var [startTime, endTime] = timeDuration.split(" - ");
+      startTime = parseTime(startTime);
+      endTime = parseTime(endTime);
 
-            // Add result
-            positionShifts[position] = (!positionShifts[position]) ? [] : positionShifts[position];
-            positionShifts[position].push({
-                user: user,
-                start: startTime,
-                end: endTime,
-            });
-        }
+      // Add result
+      positionShifts[position] = !positionShifts[position]
+        ? []
+        : positionShifts[position];
+      positionShifts[position].push({
+        user: user,
+        start: startTime,
+        end: endTime,
+      });
     }
+  }
 
-    return positionShifts;
+  return positionShifts;
 }
 
 // Where do people work?
 function getPositions(allShifts) {
-    const positions = new Set();
-    for (var [name, shifts] of Object.entries(allShifts)) {
-        shifts.forEach(function (shift) {
-            const posName = shift.innerText.split(/\d(a|p)/).at(-1).trim();
-            positions.add(posName);
-        });
-    }
+  const positions = new Set();
+  for (var [name, shifts] of Object.entries(allShifts)) {
+    shifts.forEach(function (shift) {
+      const posName = shift.innerText
+        .split(/\d(a|p)/)
+        .at(-1)
+        .trim();
+      positions.add(posName);
+    });
+  }
 
-    return [...positions];
+  return [...positions];
 }
 
 // *******************************************
@@ -145,57 +155,75 @@ function getPositions(allShifts) {
 // OTHERWISE THINGS WILL BREAK
 // *******************************************
 async function getShifts(e) {
-    // Get days of week
-    const daysContainer = document.querySelector("div.week-view-calendar-header");
-    const days = daysContainer.getElementsByClassName("user-day-header-text");
+  // Get days of week
+  const daysContainer = document.querySelector("div.week-view-calendar-header");
+  const days = daysContainer.getElementsByClassName("user-day-header-text");
 
-    // Get number of users
-    const res = await (
-        await fetch("https://app.connecteam.com/api/UserDashboard/Chat/Users/", {
-            "referrer": "https://app.connecteam.com/index.html",
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
-            "credentials": "include"
-        })
-    ).json();
-    const users = res.data.users;
+  // Get number of users
+  const res = await (
+    await fetch("https://app.connecteam.com/api/UserDashboard/Chat/Users/", {
+      referrer: "https://app.connecteam.com/index.html",
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: null,
+      method: "GET",
+      mode: "cors",
+      credentials: "include",
+    })
+  ).json();
+  const users = res.data.users;
 
-    // Get start of grid
-    const gridContainer = document.querySelector("div.week-view-calendar-body");
-    var grid = [...gridContainer.querySelectorAll("div[data-index]")].filter(function (elem) {
-        return elem.innerText != "Open to claim";
-    });
-
-    // Get rest of users
-    while (grid.length != users.length) {
-        const scrollContainer = document.querySelector("div.shift-scheduler-user-instance");
-        const scrollHeight = grid.slice(-5).reduce(function (sum, elem) {
-            return sum + elem.scrollHeight * 2;
-        }, 0);
-
-        scrollContainer.scrollBy(0, scrollHeight);
-
-        await sleep(100);
-
-        const subgrid = [...gridContainer.querySelectorAll("div[data-index]")]
-        subgrid.forEach(function (elem) {
-            if (!grid.includes(elem) && elem.innerText != "Open to claim") {
-                grid.push(elem);
-            }
-        });
+  // Get start of grid
+  const gridContainer = document.querySelector("div.week-view-calendar-body");
+  var grid = [...gridContainer.querySelectorAll("div[data-index]")].filter(
+    function (elem) {
+      return elem.innerText != "Open to claim";
     }
+  );
 
-    // Return only ones for current day
-    var shifts = {};
-    grid.forEach(function (row) {
-        const name = row.firstChild.querySelector("div.multi-user-row-header").lastChild.innerText;
-        const times = [...row.firstChild.querySelector("div.today-box").querySelectorAll("div.week-shift")];
-        shifts[name] = times;
+  // Get rest of users
+  // Limit iterations to prevent infinite loop when using different schedules
+  var maxIterations = 100;
+  var iterations = 0;
+  while (grid.length != users.length) {
+    const scrollContainer = document.querySelector(
+      "div.shift-scheduler-user-instance"
+    );
+    const scrollHeight = grid.slice(-5).reduce(function (sum, elem) {
+      return sum + elem.scrollHeight * 2;
+    }, 0);
+
+    scrollContainer.scrollBy(0, scrollHeight);
+	
+    await sleep(100);
+
+    const subgrid = [...gridContainer.querySelectorAll("div[data-index]")];
+    subgrid.forEach(function (elem) {
+      if (!grid.includes(elem) && elem.innerText != "Open to claim") {
+        grid.push(elem);
+      }
     });
+	if (iterations == maxIterations) {
+		break;
+	}
+	else {
+		iterations = iterations + 1;
+	}
+  }
 
-    return shifts;
+  // Return only ones for current day
+  var shifts = {};
+  grid.forEach(function (row) {
+    const name = row.firstChild.querySelector("div.multi-user-row-header")
+      .lastChild.innerText;
+    const times = [
+      ...row.firstChild
+        .querySelector("div.today-box")
+        .querySelectorAll("div.week-shift"),
+    ];
+    shifts[name] = times;
+  });
+
+  return shifts;
 }
 
 // Run
