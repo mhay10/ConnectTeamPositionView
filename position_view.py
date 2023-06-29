e1d33224b9d499373fcb2988385c32804c897fff
Aclamendo <davidjones@ycre.net>
2023-06-29 11:01:11 -0600

    Comprehensive refactor - Created main function that handles primary logic - Added start and end times to plot rectangles   - Text is placed outside the rectangle   - Unless the text will crash with the edge of the plot     - In this case the time will be inside the rectangle

--- position_view.py
+++ position_view.py
@@ -1,22 +1,8 @@
-import matplotlib.colors as mcolors
-import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import datetime as dt
 import pandas as pd
 import numpy as np
 import sys
-
-# Get command line args
-if len(sys.argv) != 2:
-    print("Usage: python position_view.py <file>")
-    sys.exit(1)
-
-# Read the df from the CSV file
-df = pd.read_csv(
-    sys.argv[1],
-    header=None,
-    names=["Position", "Name", "Start", "End"],
-)
-
 
 # Create color for each position
 def get_lexographic_value(s: str):
@@ -29,7 +15,7 @@ def get_lexographic_value(s: str):
 
     return total
 
-
+# RGB Value generator
 def get_color(val: int):
     val_range = (0, np.iinfo(np.uint8).max)
     rgb_range = (0, 255)
@@ -41,70 +27,140 @@ def get_color(val: int):
     return (r / 255, g / 255, b / 255)
 
 
-position_colors = {
-    pos: get_color(get_lexographic_value(pos)) for pos in df.Position.values
-}
 
-# Create a figure and an axis object
-fig, ax = plt.subplots(figsize=(15, 8))
+def main(argv):
+    # Verify input file was defined
+    if len(sys.argv) != 2:
+        print("Usage: python position_view.py <file>")
+        sys.exit(1)
 
-# Set grid formatting
-ax.grid(which="major", axis="x", linestyle="solid", alpha=0.25, color="gainsboro")
-ax.grid(
-    which="minor", axis="x", linestyle=":", linewidth=0.5, alpha=0.5, color="gainsboro"
-)
-ax.tick_params(axis="x")
-
-ax.grid(which="major", axis="y", linestyle="--", alpha=0.25, color="gainsboro")
-
-# Loop through the rows of the df and plot a horizontal bar for each row
-for i, row in df.iterrows():
-    shifts = df[df.Position == row.Position]
-    start = pd.to_datetime(row["Start"])
-    end = pd.to_datetime(row["End"])
-    duration = end - start
-
-    bar = ax.broken_barh(
-        [(start.hour + start.minute / 60, duration.total_seconds() / 3600)],
-        (i - 0.25, 0.5),
-        facecolors=position_colors[row.Position] + (0.25,),
-        edgecolors=position_colors[row.Position] + (0.5,),
-    )
-    ax.text(
-        (start.hour + start.minute / 60) + duration.total_seconds() / 7200,
-        i + 0.075,
-        row["Name"],
-        fontsize=8,
-        weight="bold",
-        ha="center",
-        va="center",
-        color="black",
+    # Read the shifts from the CSV file into a DataFrame
+    shiftData = pd.read_csv(
+        sys.argv[1],
+        header=None,
+        names=["Position", "Name", "Start", "End"],
     )
 
-# Set the y-ticks and labels to the positions
-ax.set_yticks(range(len(df)))
-ax.set_yticklabels(df["Position"])
+    # Create dictionary for all position colors
+    position_colors = {
+        pos: get_color(get_lexographic_value(pos)) for pos in shiftData.Position.values
+    }
 
-# Set the major x-ticks every 30 mins
-xticks = np.arange(7.5, 22.5, 0.5)
-ax.set_xticks(xticks)
-xlabels = [
-    f"{int(t) % 12 if int(t) != 12 else 12}:{'00' if t.is_integer() else '30'} {'AM' if t < 12 else 'PM'}"
-    for t in xticks
-]
-ax.set_xticklabels(xlabels)
-ax.set_xlim(xticks[0], xticks[-1])
-plt.xticks(rotation=45, ha="right")
+    # Create a figure and an axis object
+    fig, ax = plt.subplots(figsize=(15, 8))
 
-# Set the minor x-ticks every 15 mins
-minor_xticks = np.arange(7.75, 22.5, 0.5)
-ax.set_xticks(minor_xticks, minor=True)
+    # Set grid formatting
+    # X axis formatting
+    ax.grid(which="major", axis="x", linestyle="solid", alpha=0.25, color="gainsboro")
+    ax.grid(which="minor", axis="x", linestyle=":", linewidth=0.5, alpha=0.5, color="gainsboro")
+    ax.tick_params(axis="x")
 
-# Set the title and axis labels
-ax.set_title("Position View")
+    # Y axis formatting
+    ax.grid(which="major", axis="y", linestyle="--", alpha=0.25, color="gainsboro")
 
-# Invert y-axis
-ax.invert_yaxis()
+    # Loop through the rows of the shifts and plot a horizontal bar for each row
+    for i, row in shiftData.iterrows():
+        shifts = shiftData[shiftData.Position == row.Position]
+        start = pd.to_datetime(row["Start"])
+        end = pd.to_datetime(row["End"])
+        duration = end - start
 
-# Show the plot
-plt.show()
+        bar = ax.broken_barh(
+            [(start.hour + start.minute / 60, duration.total_seconds() / 3600)],
+            (i - 0.25, 0.5),
+            facecolors=position_colors[row.Position] + (0.25,),
+            edgecolors=position_colors[row.Position] + (0.5,),
+        )
+
+        
+        # Name and time text
+        # Start time options to prevent text crash with edge of plot
+        if start.hour + start.minute / 60 < 8.5: 
+            ax.text(
+                start.hour + start.minute / 60, # start time
+                i+0.075,
+                dt.datetime.strftime(start, "%I:%M %p"),
+                fontsize=8,
+                weight="bold",
+                ha="left",
+                va="center",
+                color="black",
+            )
+        else:
+            ax.text(
+                start.hour + start.minute / 60, # start time
+                i+0.075,
+                dt.datetime.strftime(start, "%I:%M %p"),
+                fontsize=8,
+                weight="bold",
+                ha="right",
+                va="center",
+                color="black",
+            )
+        ax.text(
+            start.hour + start.minute / 60 + duration.total_seconds() / 7200,
+            i + 0.075,
+            str(row["Name"]),
+            fontsize=8,
+            weight="bold",
+            ha="center",
+            va="center",
+            color="black",
+        )
+
+        # End time options to prevent crashing with edge of plot
+        if end.hour + end.minute / 60 > 21.5:
+            ax.text(
+                end.hour + end.minute / 60, # end time
+                i+0.075,
+                dt.datetime.strftime(end, "%I:%M %p"),
+                fontsize=8,
+                weight="bold",
+                ha="right",
+                va="center",
+                color="black",
+            )
+        else:    
+            ax.text(
+                end.hour + end.minute / 60, # end time
+                i+0.075,
+                dt.datetime.strftime(end, "%I:%M %p"),
+                fontsize=8,
+                weight="bold",
+                ha="left",
+                va="center",
+                color="black",
+            )
+
+    # Set the y-ticks and labels to the positions
+    ax.set_yticks(range(len(shiftData)))
+    ax.set_yticklabels(shiftData["Position"])
+
+    # Set the major x-ticks every 30 mins
+    xticks = np.arange(7.5, 22.5, 0.5)
+    ax.set_xticks(xticks)
+    xlabels = [
+        f"{int(t) % 12 if int(t) != 12 else 12}:{'00' if t.is_integer() else '30'} {'AM' if t < 12 else 'PM'}"
+        for t in xticks
+    ]
+    ax.set_xticklabels(xlabels)
+    ax.set_xlim(xticks[0], xticks[-1])
+    plt.xticks(rotation=45)
+
+    # Set the minor x-ticks every 15 mins
+    minor_xticks = np.arange(7.75, 22.5, 0.5)
+    ax.set_xticks(minor_xticks, minor=True)
+
+    # Set the title and axis labels
+    ax.set_title("Position View")
+
+    # Invert y-axis
+    ax.invert_yaxis()
+
+    # Show the plot
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main(sys.argv)
